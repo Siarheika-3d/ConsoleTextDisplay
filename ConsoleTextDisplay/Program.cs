c07cddedef877ad18067c6bcf0be58b4d245dbd6
Serge P <serge.prokhorov@hypernovateam.com>
2023-08-10 22:24:43 +0200

    Add project files.

---
+++ ConsoleTextDisplay/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleTextDisplay
+
+
+{
+    /// <summary>
+    /// Somehow the class name should reflect purpose or the main meaning of code of the class.  And this is my very first lines of code.
+    /// </summary>
+    class FirstTextLine
+    {
+        /// <summary>
+        /// Code for just one text line in console. "Hello Word" - the first codding practice for many developers.
+        /// </summary>
+        /// <param name="args">Don`t know what "args" mean yet. Some kind of the main part of executable code. </param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World !!!");  // Text in console we will see.
+                                                  
+
+            Console.ReadKey();                     // Awaiting for user action (pressing any key) to exit console.
+
+
+            /*
+             “A human being unable to have a meaningful impact on the world ceases to exist.”
+                                                   ― David Graeber, Bullshit Jobs: A Theory
+             */
+        }
+    }
+}
